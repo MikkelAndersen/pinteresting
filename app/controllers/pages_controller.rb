7c1a818b9804e8e4987f3fc3a3bc6852f0be0665
Mikkel Andersen <skrivtilmikkel@hotmail.com>
2019-06-30 22:29:51 +0200

    Første sider tilføjet

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  def home
+  end
+  def omos
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "omos" => "pages#omos"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
